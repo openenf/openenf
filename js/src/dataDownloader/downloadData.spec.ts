9e0faf9fe851fc2905ad778cde46945c7f849dec
Chris Hodges <chris@chrishodges.me.uk>
2023-03-31 18:39:12 +0100

    Moved datadownloader test to long running folder

--- js/src/dataDownloader/downloadData.spec.ts
+++ js/src/dataDownloader/downloadData.spec.ts
@@ -20,30 +20,4 @@ describe('downloadData', () => {
                 expect(result.endsWith('.config/openenf')).toBe(true);
         }
     })
-    it('can download data file', (done) => {
-        const filePath = path.join(getENFDataDirectory(),"xx.freqdb");
-        if (fs.existsSync(filePath)) {
-            fs.rmSync(filePath);
-        }
-        downloadData("XX").then(() => {
-            expect(fs.existsSync(filePath)).toBe(true);
-            const fileHash = crypto.createHash('md5');
-            const stream = fs.createReadStream(filePath);
-
-            stream.on('data', function(data) {
-                fileHash.update(data);
-            });
-
-            stream.on('end', function() {
-                const md5sum = fileHash.digest('hex');
-                expect(md5sum).toBe('ddc138b743874940c9577e03df5b21db')
-                fs.rmSync(filePath);
-                done();
-            });
-
-            stream.on('error', function(err) {
-                console.error(`Error reading file: ${err}`);
-            });
-        })
-    })
 })
