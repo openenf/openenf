8d3bece6a3ea0f26acf627ede5d13ba3e1d145c0
Chris Hodges <chris@chrishodges.me.uk>
2023-04-03 17:34:52 +0100

    Added basic parameterisation

--- js/bin/index.js
+++ js/bin/index.js
@@ -1,2 +1,25 @@
 #!/usr/bin/env node
-console.log('Hi');
+
+const parseDateWithError = opt => {
+    const result = Date.parse(opt);
+    if (isNaN(result)) {
+        throw new Error(`Unable to parse '${opt}' as date.`)
+    }
+    return result;
+}
+
+const argv = require('yargs/yargs')(process.argv.slice(2))
+    .demandCommand(1)
+    .array('grids')
+    .alias('g','grids')
+    .default('grids',['DE','GB'])
+    .alias('s','start')
+    .coerce('start', parseDateWithError)
+    .alias('e','end')
+    .coerce('end', parseDateWithError)
+    .argv;
+
+const filepath = argv._[0];
+
+console.log(argv);
+console.log('filepath', filepath);
