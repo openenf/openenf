31791696df099c9bb6b5cd3a0a65020e80719c17
Chris Hodges <chris@chrishodges.me.uk>
2023-04-20 21:31:10 +0100

    Fixed long running tests

--- js/global-longrunning-setup.js
+++ js/global-longrunning-setup.js
@@ -1,14 +1,15 @@
 const { TcpLookupServerController } = require('./src/tcpClient/tcpLookupServerController');
 const { getDefaultExecutablePath } = require('./src/tcpClient/tcpClientUtils');
 const path = require("path");
+const {verifyApplicationData} = require("./src/dataDownloader/downloadData");
 
 const port = 49170;
 let serverController;
 
 module.exports = async function globalSetup() {
     if (!(await TcpLookupServerController.ServerRunningOnPort(port))) {
-        const testGrid = path.resolve("test/testFreqDbs/GB_50_Jan2014.freqdb");
-        serverController = new TcpLookupServerController(port, getDefaultExecutablePath(),[testGrid]);
+        const grids = await verifyApplicationData();
+        serverController = new TcpLookupServerController(port, getDefaultExecutablePath(),grids);
         await serverController.start();
         global.serverController = serverController;
     }
